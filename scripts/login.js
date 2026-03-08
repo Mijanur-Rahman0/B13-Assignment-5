a853ea619db868b8148a8d86db1b14fdac069781
Mijanur Rahman <mijanurrahman0645@gmail.com>
2026-03-08 14:26:26 +0600

    Login page added successfully

---
+++ scripts/login.js
@@ -0,0 +1,24 @@
+document.getElementById('login-btn').addEventListener('click', () => {
+    const inputName = document.getElementById('input-name');
+    const userName = inputName.value;
+    // console.log(userName)
+
+    const inputPass = document.getElementById('input-pass');
+    const password = inputPass.value;
+    // console.log(password)
+
+    if(userName === 'admin' && password === 'admin123'){
+        alert('login Success');
+        window.location.assign('./home.html');
+    }else{
+        alert('Login failed');
+        return;
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'Enter'){
+        document.getElementById('input-name')
+        document.getElementById('login-btn').click();
+    }
+});
